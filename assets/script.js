762149ee38017a1a0ebf4ea7c7832b7067e278e9
mvv1790 <mvv1790@gmail.com>
2023-01-22 10:59:17 +0000

    Added choices const, computer Play function and scores

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+let round = 1
+let playerScore = 0
+let computerScore = 0
+let computerChoice
+
+const choices = ["rock", "paper", "scissors", "lizard", "spock"]
+
+function computerPlay() {
+    return choices[Math.floor(Math.random() * 5) + 1]
+}
+
+function playerTurn(){
+    if (!player) return null
+}
+
 function rockFunction() {
     console.log("rock")
 }
@@ -17,3 +32,11 @@ function lizardFunction() {
 function spockFunction() {
     console.log('spock')
 }
+
+function gameState() {
+    playerScore = 0
+    computerScore = 0
+    round = 1 
+}
+
+function
