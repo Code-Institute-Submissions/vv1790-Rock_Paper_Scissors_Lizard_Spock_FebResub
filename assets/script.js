e92a0cdc8acb0535d977f443d83d6c7275464d7f
mvv1790 <mvv1790@gmail.com>
2023-01-22 10:49:59 +0000

    Added event listeners and button functions

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+function rockFunction() {
+    console.log("rock")
+}
+
+function paperFunction() {
+    console.log('paper')
+}
+
+function scissorsFunction() {
+    console.log('scissors')
+}
+
+function lizardFunction() {
+    console.log('lizard')
+}
+
+function spockFunction() {
+    console.log('spock')
+}
