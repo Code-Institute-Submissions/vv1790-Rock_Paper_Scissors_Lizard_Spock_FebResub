d84d9abde64e01ad6e3ae1dbb75f1e80beb56ff0
mvv1790 <mvv1790@gmail.com>
2023-02-03 16:33:14 +0000

    Refactoring javascript code, created button functions

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,29 @@
 const displayPlayerChoice = document.getElementById('player_choice')
 const displayComputerChoice = document.getElementById('computer_choice')
 const displayPlayerScore = document.getElementById('player_score')
-const displayComputerScore = document.getElementById('computer_score')
-const choiceRange = document.querySelectorAll('button')
+const displayComputerScore = document.getElementById('computer_choice')
 
-let playerChoice
-let computerChoice
-let playerScore = 0
-let computerScore = 0
+let playerChoice 
+let computerChoice 
+let playerScore 
+let computerScore 
 
-
-const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock']
-
-choiceRange.forEach(choiceRange => choiceRange.addEventListener('click', (e) => {
-    playerChoice = e.target.id
-    displayPlayerChoice.innerHTML = playerChoice
-    displayComputerChoice.innerHTML = computerPlay()
-    displayPlayerScore.innerHTML = playerScore
-    displayComputerScore.innerHTML = computerScore
-    getResult()
-    gameState()
-}))
-
-function computerPlay() {
-    return choices[Math.floor(Math.random() * 5) + 1]
+function rockFunction() {
+    document.getElementById('player_choice').innerHTML = "Rock"
 }
 
-function gameState() {
-    playerScore = 0
-    computerScore = 0
+function paperFunction() {
+    document.getElementById('player_choice').innerHTML = "Paper"
 }
 
-function getResult() {
-    if (
-        (playerChoice === "rock" && computerChoice === "scissors") ||
-        (playerChoice === "rock" && computerChoice === "lizard") ||
-        (playerChoice === "paper" && computerChoice === "rock") ||
-        (playerChoice === "paper" && computerChoice === "spock") ||
-        (playerChoice === "scissors" && computerChoice === "paper") ||
-        (playerChoice === "scissors" && computerChoice === "lizard") ||
-        (playerChoice === "lizard" && computerChoice === "paper") ||
-        (playerChoice === "lizard" && computerChoice === "spock") ||
-        (playerChoice === "spock" && computerChoice === "scissors") ||
-        (playerChoice === "spock" && computerChoice === "rock")
-
-    ) {
-        playerScore = playerScore + 1
-    } else if (
-        (playerChoice === "scissors" && computerChoice === "rock") ||
-        (playerChoice === "lizard" && computerChoice === "rock") ||
-        (playerChoice === "rock" && computerChoice === "paper") ||
-        (playerChoice === "spock" && computerChoice === "paper") ||
-        (playerChoice === "paper" && computerChoice === "scissors") ||
-        (playerChoice === "lizard" && computerChoice === "scissors") ||
-        (playerChoice === "scissors" && computerChoice === "lizard") ||
-        (playerChoice === "rock" && computerChoice === "lizard") ||
-        (playerChoice === "paper" && computerChoice === "spock") ||
-        (playerChoice === "lizard" && computerChoice === "spock")
-    ) {
-        computerScore = computerScore + 1
-    } else {
-        
-    }
-
+function scissorsFunction() {
+    document.getElementById('player_choice').innerHTML = "Scissors"
 }
 
+function lizardFunction() {
+    document.getElementById('player_choice').innerHTML = "Lizard"
+}
 
+function spockFunction() {
+    document.getElementById('player_choice').innerHTML = "Spock"
+}
