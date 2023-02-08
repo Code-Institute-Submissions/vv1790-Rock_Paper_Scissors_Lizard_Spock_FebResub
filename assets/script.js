d7a0987e5945b4d24997670f4c45b60dce0efb7a
mvv1790 <mvv1790@gmail.com>
2023-02-08 23:41:26 +0000

    Refactoring button functions

--- assets/script.js
+++ assets/script.js
@@ -1,59 +1,23 @@
+// const btn  = document.querySelector('button')
+const rockBtn = document.querySelector('.rock')
+const paperBtn = document.querySelector('.paper')
+const scissorsBtn = document.querySelector('.scissors')
+const lizardBtn = document.querySelector('.lizard')
+const spockBtn = document.querySelector('.spock')
+
 const displayPlayerChoice = document.getElementById('player_choice')
 const displayComputerChoice = document.getElementById('computer_choice')
-const displayPlayerScore = document.getElementById('player_score')
-const displayComputerScore = document.getElementById('computer_choice')
 
-const choices = ["rock", "paper", "scissors", "lizard", "spock"]
+const computerChoices = ['rock', 'paper' , 'scissors', 'lizard', 'spock']
+const playerChoices = [rockBtn, paperBtn, scissorsBtn, lizardBtn, spockBtn]
 
-let playerChoice 
-let computerChoice 
-let playerScore 
-let computerScore 
 
-function computerPlay() {
-    const generateChoice = Math.floor(Math.random() * 5) + 1
-
-    if (generateChoice === 1) {
-        computerChoice = 'Computer: Rock'
-      }
-      if (generateChoice === 2) {
-        computerChoice = 'Computer: Scissors'
-      }
-      if (generateChoice === 3) {
-        computerChoice = 'Computer: Paper'
-      }
-      if (generateChoice === 4) {
-        computerChoice = 'Computer: Lizard'
-      }
-      if (generateChoice === 5) {
-        computerChoice = 'Computer: Spock'
-      }
-      displayComputerChoice.innerHTML = computerChoice
-
+rockBtn.addEventListener("click", changeColor);
+      function changeColor() {
+        displayPlayerChoice.innerHTML = playerChoices
 }
 
-function rockFunction() {
-    document.getElementById('player_choice').innerHTML = "Player: Rock"
-    computerPlay()
+paperBtn.addEventListener("click", changeColor);
+      function changeColor() {
+        displayPlayerChoice.innerHTML = 'paper'
 }
-
-function paperFunction() {
-    document.getElementById('player_choice').innerHTML = "Player: Paper"
-    computerPlay()
-}
-
-function scissorsFunction() {
-    document.getElementById('player_choice').innerHTML = "Player: Scissors"
-    computerPlay()
-}
-
-function lizardFunction() {
-    document.getElementById('player_choice').innerHTML = "Player: Lizard"
-    computerPlay()
-}
-
-function spockFunction() {
-    document.getElementById('player_choice').innerHTML = "Player: Spock"
-    computerPlay()
-}
-
